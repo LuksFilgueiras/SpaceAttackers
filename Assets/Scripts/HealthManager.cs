e3b68a6d136e99fb9df1c2a5b06123af2f406652
Lucas Filgueiras <lucasflacko@gmail.com>
2023-10-26 13:23:20 -0300

    feat/hit animation + death explosion vfx

--- Assets/Scripts/HealthManager.cs
+++ Assets/Scripts/HealthManager.cs
@@ -6,6 +6,8 @@ public class HealthManager : MonoBehaviour
 {
     [SerializeField] private int actualHealth;
     [SerializeField] private int maxHealth;
+    [SerializeField] private GameObject explosionVFX;
+    [SerializeField] private Animator animator;
 
     void Awake(){
         actualHealth = maxHealth;
@@ -13,8 +15,11 @@ public class HealthManager : MonoBehaviour
 
     public void TakeDamage(int damage){
         actualHealth -= damage;
+        animator.SetTrigger("Hit");
 
         if(actualHealth <= 0){
+            GameObject explosionVFXInstance = Instantiate(explosionVFX, transform.position, Quaternion.identity);
+            Destroy(explosionVFXInstance, 0.45f);
             Destroy(gameObject);
         }
     }
